13a1dbae4f6d52f468d8be1fd7dc2f5bb0a1edaf
Turtwi <90651802+Turtwi@users.noreply.github.com>
2022-05-02 02:49:07 +0600

    Delete Week2 directory

--- Week2/src/Week2.java
+++
@@ -1,9 +0,0 @@
-public class Week2 {
-    public static void main(String[] args){
-        Person person = new Person("Ansar", "Aktobe", "+77055891542","210103367@stu.sdu.edu.kz");
-        Student student = new Student("Steff", "USA","4565545654","steff12@gmail.com","freshman");
-
-        System.out.println(person.ToString());
-        System.out.println(student.ToString());
-    }
-}
